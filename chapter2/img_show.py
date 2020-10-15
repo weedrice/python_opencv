7bf83d50d15ce5eb0eb06ada056fc5865997c632
weedrice <supp0rtyoo@gmail.com>
2020-10-15 23:19:15 +0900

    #201015 Init project & Chapter 2-1

---
+++ chapter2/img_show.py
@@ -0,0 +1,11 @@
+import cv2
+
+img_file = "../img/girl_2.jpg"
+img = cv2.imread(img_file)
+
+if img is not None:
+    cv2.imshow('IMG', img)
+    cv2.waitKey()
+    cv2.destoryAllWindows()
+else:
+    print('No image file.')
